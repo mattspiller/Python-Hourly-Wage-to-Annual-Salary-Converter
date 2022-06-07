db2d2c0734a5f23f25a8b9b735500a7f72d58170
Matthew Spiller <48130623+mattspiller@users.noreply.github.com>
2022-06-07 12:53:10 -0400

    Update wageToSalaryCalculator.py

--- wageToSalaryCalculator.py
+++ wageToSalaryCalculator.py
@@ -1,12 +1,34 @@
-print("Enter hourly wage: ", end='')
-wage = float(input())
+# Author: Matt Spiller
+# Updated: June 7, 2022
 
-print("Enter hours worked per week: ", end='')
-weeklyHours = int(input())
+def wageToSalaryCalculator():
+    print("Enter hourly wage: ", end='')
+    wage = float(input())
 
-print("Enter vacation days per year: ", end='')
-vacationDays = int(input())
+    print("Enter hours worked per week: ", end='')
+    weeklyHours = int(input())
 
-annualSalary = wage * weeklyHours * (52 - float(vacationDays)/7.0)
+    print("Enter vacation days per year: ", end='')
+    vacationDays = int(input())
 
-print("Annual Salary: ", annualSalary)
+    annualSalary = wage * weeklyHours * (52 - float(vacationDays) / 7.0)
+
+    print("Annual Salary: ", annualSalary)
+    runAgainPrompt()
+
+
+def runAgainPrompt():
+    print("Would you like to do another calculation? (y,n): ", end='')
+    runAgain = input()
+
+    while runAgain != "y" and runAgain != "n":
+        print("Response must be 'y' or 'n'. Would you like to do another calculation? (y,n): ", end='')
+        runAgain = input()
+
+    if runAgain == "y":
+        wageToSalaryCalculator()
+    else:
+        return
+
+
+wageToSalaryCalculator()
